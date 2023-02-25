639940ea76802cbaecda7ae25d1c9dfc916d4784
James Mahfouz <jamesmahfouz0@gmail.com>
2023-02-25 17:58:39 +0200

    fixed a lot of bugs and added a check email function

--- playground.js
+++ playground.js
@@ -1,18 +1,50 @@
 window.onload = function(){
-    let button = document.getElementById('register_btn') 
-    button.addEventListener('click', function(){
+    let button = document.getElementById('register_btn')
+    button.addEventListener('click', handleClick)
+
+    function handleClick() {
         let first_name = document.getElementById('f_name').value
         let last_name = document.getElementById('l_name').value
         let gender = document.getElementById('gender').value
         let email = document.getElementById('e_mail').value
         let password = document.getElementById('pass_code').value
         let conf_password = document.getElementById('re_pass_code').value
+        let last_ID = 0
+        let data = {}
 
-        alert(gender)
-    })
+        if (checkEmptyValue()) {
+            button.removeEventListener('click', handleClick)
+        }
+        else{
+            ID = generateID()
+            values = [first_name, last_name, gender, email, password]
+            data[ID] = values
+            for (var i =0 ; i<7; i++){
+                console.log(data[ID][i])
+            }
+        }
+
+        function checkEmptyValue() {
+            if (first_name.trim() == '' || last_name.trim() == '' || gender.trim() == '' || email.trim() == '' || password.trim() == '' || conf_password.trim() == '') {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        function generateID(){
+            last_ID++;
+            return last_ID;
+        }
+    }
     
-    
-    //function checkEmptyValue(){
-        //if(first_name == ''|| last_name == '' || gender == )
-    //}
+    function checkEmail(){
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            alert("Invalid email format");
+        return;
+        }
+    }
+ 
 }
