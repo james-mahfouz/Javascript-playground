31dcff39d92c12af5cd032a33d9f61893377e221
James Mahfouz <jamesmahfouz0@gmail.com>
2023-02-26 15:22:33 +0200

    the user will input his year of birth and the system will say if his age is a prime number or not

--- javascripts folder/playground.js
+++ javascripts folder/playground.js
@@ -1,9 +1,11 @@
 window.onload = function(){
     let merge_button = document.getElementById('merge_button')
     let palindrome_button = document.getElementById('palindrome_button')
+    let prime_button = document.getElementById('prime_button')
 
     merge_button.addEventListener('click', merge_numbers)
     palindrome_button.addEventListener('click', check_palindrome)
+    prime_button.addEventListener('click', check_prime_age)
 
     function merge_numbers(){
         event.preventDefault()
@@ -85,6 +87,31 @@ window.onload = function(){
             
         function palindrome(string) {
             return palindrome_helper(string, 0, string.length - 1);
+        }
+    }
+
+    function check_prime_age(){
+        let year_of_birth = document.getElementById('user_prime_input').value
+        let age = new Date().getFullYear() - parseInt(year_of_birth)
+        let display = document.getElementById('prime-output')
+        age=parseInt(age)
+        console.log(age)
+
+        if(isPrime(age)){
+            display.innerHTML = `Nice, You are ${age} years old, which is a prime number`
+        }else{
+            display.innerHTML = `Nice, You are ${age} years old, but it isn't a prime number`
+        }
+
+        function isPrime(age_input){
+            let is_prime = true
+            for(let i = 2; i<= age/2; i++){
+                if(age % i==0){
+                    is_prime=false
+                    break;
+                }
+            }
+            return is_prime
         }
     }
 }
